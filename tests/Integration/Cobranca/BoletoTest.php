0e299af5e15fc3e8d33faa4d6ce4f9fb62b982ef
Vinícius de Senna <sennadevinicius@gmail.com>
2026-09-22 19:17:38 -0300

    Gerar boleto especifico

--- tests/Integration/Cobranca/BoletoTest.php
+++ tests/Integration/Cobranca/BoletoTest.php
@@ -81,7 +81,7 @@ class BoletoTest extends CobrancaTestCase
                 'codigoCarteiraCobranca' => 1,
             ],
             'documento' => [
-                'numeroDocumento' => rand(100000, 999999),
+                'numeroDocumento' => '100001',
                 'descricaoDocumento' => 'Mensalidade',
                 'especieDocumento' => 1,
             ],
